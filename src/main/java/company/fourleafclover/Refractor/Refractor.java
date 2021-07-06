1d6bbf202f6f1d289881d0b6ed7dbf6e827cdd33
TheBozzz34 <lskywalker416@gmail.com>
2021-07-06 07:33:49 -0600

    Update Sentry.io

--- src/main/java/company/fourleafclover/Refractor/Refractor.java
+++ src/main/java/company/fourleafclover/Refractor/Refractor.java
@@ -4,12 +4,28 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerLoginEvent;  
+import io.sentry.Sentry;
+import java.lang.Exception;
+
 
 
 public class Refractor extends JavaPlugin {
     @Override
     public void onEnable() {
-        getLogger().info("Refractor 1.3 Is Loaded");
+        getLogger().info("Refractor 1.4 Is Loaded");
+        Sentry.init(options -> {
+            options.setDsn("https://6ed02058198b475ea5c08ee012467b5b@o561860.ingest.sentry.io/5851198");
+            // Set traces_sample_rate to 1.0 to capture 100% of transactions for performance monitoring.
+            // We recommend adjusting this value in production.
+            options.setTracesSampleRate(1.0);
+            // When first trying Sentry it's good to see what the SDK is doing:
+            options.setDebug(true);
+          });
+          try {
+            throw new Exception("This is a test.");
+          } catch (Exception e) {
+            Sentry.captureException(e);
+          }
         
        
         
@@ -20,7 +36,7 @@ public class Refractor extends JavaPlugin {
     }
     @Override
     public void onDisable() {
-        getLogger().info("Refractor 1.3 is Unloaded");
+        getLogger().info("Refractor 1.4 is Unloaded");
     }
 
 
