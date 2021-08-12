67b1469cf1646cbcf3fd8813368bf674d5a9cea1
thebozzz34 <lskywalker416@gmail.com>
2021-08-12 09:14:34 -0600

    Create execption command

---
+++ src/main/java/company/fourleafclover/Refractor/CommandKit.java
@@ -0,0 +1,27 @@
+package company.fourleafclover.Refractor;
+
+import io.sentry.Sentry;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CommandKit implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            try {
+                throw new Exception("This is a test.");
+            } catch (Exception e) {
+                Sentry.captureException(e);
+            }
+            // Here we need to give items to our player
+        }
+
+        // If the player (or console) uses our command correct, we can return true
+        return true;
+    }
+
+
+}
--- src/main/java/company/fourleafclover/Refractor/Refractor.java
+++ src/main/java/company/fourleafclover/Refractor/Refractor.java
@@ -1,7 +1,12 @@
 package company.fourleafclover.Refractor;
 
 import org.bstats.bukkit.Metrics;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import io.sentry.Sentry;
+import java.lang.Exception;
 
 
 public class Refractor extends JavaPlugin {
@@ -10,6 +15,15 @@ public class Refractor extends JavaPlugin {
         getLogger().info("Refractor 1.5 Is Loaded");
         int pluginId = 12406; // <-- Replace with the id of your plugin!
         Metrics metrics = new Metrics(this, pluginId);
+        this.getCommand("error").setExecutor(new CommandKit());
+        Sentry.init(options -> {
+            options.setDsn("https://438653d78f4044eabce86bfac30ec13b@o561860.ingest.sentry.io/5904137");
+            // Set traces_sample_rate to 1.0 to capture 100% of transactions for performance monitoring.
+            // We recommend adjusting this value in production.
+            options.setTracesSampleRate(1.0);
+            // When first trying Sentry it's good to see what the SDK is doing:
+            options.setDebug(true);
+        });
 
 
     }
@@ -17,6 +31,8 @@ public class Refractor extends JavaPlugin {
     public void onDisable() {
         getLogger().info("Refractor 1.5 is Unloaded");
     }
+
+
 
 
 
