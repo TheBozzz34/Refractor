5a5e37d1d015a2aa55f0fc95dcf74ffd06eb1260
TheBozzz34 <lskywalker416@gmail.com>
2021-08-12 16:30:21 -0600

    Setup discord login

--- src/main/java/company/fourleafclover/Refractor/discord.java
+++ src/main/java/company/fourleafclover/Refractor/discord.java
@@ -3,21 +3,19 @@ package company.fourleafclover.Refractor;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.bukkit.plugin.java.JavaPlugin;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.javacord.api.entity.channel.TextChannel;
 
 public class discord {
 
     public static void main(String[] args) {
         // Insert your bot's token here
-        String token = "your token";
+        Dotenv dotenv = Dotenv.load();
+        String token = dotenv.get("TOKEN");
 
         DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
 
-        // Add a listener which answers with "Pong!" if someone writes "!ping"
-        api.addMessageCreateListener(event -> {
-            if (event.getMessageContent().equalsIgnoreCase("!ping")) {
-                event.getChannel().sendMessage("Pong!");
-            }
-        });
+
 
         // Print the invite url of your bot
         System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
