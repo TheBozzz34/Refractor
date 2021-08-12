5a553937cba31857103021cf70421235c1eaa7dc
TheBozzz34 <lskywalker416@gmail.com>
2021-08-11 21:03:13 -0600

    Init bstats

--- src/main/java/company/fourleafclover/Refractor/Refractor.java
+++ src/main/java/company/fourleafclover/Refractor/Refractor.java
@@ -1,31 +1,21 @@
 package company.fourleafclover.Refractor;
 
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.event.Listener;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerLoginEvent;  
-
 
 
 public class Refractor extends JavaPlugin {
     @Override
     public void onEnable() {
-        getLogger().info("Refractor 1.4 Is Loaded");
-       // int pluginId = 11941; // <-- Replace with the id of your plugin!
-      //  Metrics metrics = new Metrics(this, pluginId);
-        
-          
-        
-       
-        
-      
-    
+        getLogger().info("Refractor 1.5 Is Loaded");
+        int pluginId = 12406; // <-- Replace with the id of your plugin!
+        Metrics metrics = new Metrics(this, pluginId);
 
-     
+
     }
     @Override
     public void onDisable() {
-        getLogger().info("Refractor 1.4 is Unloaded");
+        getLogger().info("Refractor 1.5 is Unloaded");
     }
 
 
