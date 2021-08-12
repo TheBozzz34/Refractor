7e1ccedf149f3b53bfd3f133ed4ca26e13382189
thebozzz34 <lskywalker416@gmail.com>
2021-08-12 12:13:18 -0600

    Fix commands class

--- src/main/java/company/fourleafclover/Refractor/Refractor.java
+++ src/main/java/company/fourleafclover/Refractor/Refractor.java
@@ -17,7 +17,7 @@ public class Refractor extends JavaPlugin {
         int pluginId = 12406; // <-- Replace with the id of your plugin!
         Metrics metrics = new Metrics(this, pluginId);
         commands commands = new commands();
-        getCommand("error").setExecutor(commands);
+        getCommand("generror").setExecutor(commands);
         Sentry.init(options -> {
             options.setDsn("https://438653d78f4044eabce86bfac30ec13b@o561860.ingest.sentry.io/5904137");
             // Set traces_sample_rate to 1.0 to capture 100% of transactions for performance monitoring.
