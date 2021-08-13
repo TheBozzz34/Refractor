de76d0d738ff8a0885ba5e4cb033b8ea51f3249a
TheBozzz34 <lskywalker416@gmail.com>
2021-08-12 20:17:57 -0600

    Fix dsc.java
    change to getCommand("dsc").setExecutor(new dsc());

--- src/main/java/company/fourleafclover/Refractor/dsc.java
+++ src/main/java/company/fourleafclover/Refractor/dsc.java
@@ -2,20 +2,25 @@ package company.fourleafclover.Refractor;
 
 import java.lang.Exception;
 import io.sentry.Sentry;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class dsc implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (!(sender instanceof Player)) { return true; }
         Player player = (Player) sender;
+        Logger logger = LoggerFactory.getLogger(dsc.class);
 
-        //generate execution
+
         if(cmd.getName().equalsIgnoreCase("dsc")) {
-            sender.sendMessage("Join our community at: dsc.gg/t0ast!");
+            player.sendMessage(ChatColor.GREEN+"Join our discord at dsc.gg/t0ast!");
+            logger.info(cmd.getName());
         }
 
 
